fc8be532d39937c50f40b6041327cd98c66917d7
sriharshams <sriharshams@gmail.com>
2015-08-10 02:18:49 -0700

    Updated audit.py

--- audit.py
+++ audit.py
@@ -71,19 +71,21 @@ def update_name(name, mapping):
     m = street_type_re.search(name)
     if m:
         street_type = m.group()
-        name = name.replace(street_type, mapping[street_type])
+        if mapping.has_key(street_type):
+            name = name.replace(street_type, mapping[street_type])
     return name
 
 
 def test():
     st_types = audit(OSMFILE)
     #assert len(st_types) == 3
-    pprint.pprint(dict(st_types))
+    #pprint.pprint(dict(st_types))
 
-    # for st_type, ways in st_types.iteritems():
-    #     for name in ways:
-    #         better_name = update_name(name, mapping)
-    #         print name, "=>", better_name
+    for st_type, ways in st_types.iteritems():
+         for name in ways:
+             better_name = update_name(name, mapping)
+             if name!=better_name:
+                print name, "=>", better_name
     #         if name == "West Lexington St.":
     #             assert better_name == "West Lexington Street"
     #         if name == "Baldwin Rd.":
